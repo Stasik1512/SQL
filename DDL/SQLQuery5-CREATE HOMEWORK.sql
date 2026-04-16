0da11b51ed87b1814e3c55f64b9aa8472d6aec79
Nekit K. <14nikita@list.ru>
2026-04-16 16:47:20 +0500

    ALL IN ONE DONE

--- DDL/SQLQuery5-CREATE HOMEWORK.sql
+++ DDL/SQLQuery5-CREATE HOMEWORK.sql
@@ -3,7 +3,7 @@ USE P_421_DDL;
 
 CREATE TABLE Homework
 (
-	lesson BIGINT CONSTRAINT FK_Homework_Schedule FOREIGN KEY REFERENCES Schedule(lesson_id),
+	lesson BIGINT PRIMARY KEY CONSTRAINT FK_Homework_Schedule FOREIGN KEY REFERENCES Schedule(lesson_id),
 	task NVARCHAR(1024) NOT NULL,
 	[date] VARBINARY(MAX) NULL,
 	dedline DATE NOT NULL,
@@ -11,9 +11,11 @@ CREATE TABLE Homework
 
 CREATE TABLE HWresults 
 (
-	student INT PRIMARY KEY,
+	student INT CONSTRAINT FK_HWresults_Students FOREIGN KEY REFERENCES Students(student_id),
 	lesson BIGINT CONSTRAINT FK_HWresults_Schedule FOREIGN KEY REFERENCES Schedule(lesson_id),
+	PRIMARY KEY (student, lesson),
 	[date] DATE NOT NULL,
 	answer NVARCHAR(1024) NOT NULL,
-	grade TINYINT CONSTRAINT CK_Grade CHECK(grade > 0 AND grade <= 12)
+	grade TINYINT CONSTRAINT CK_HWresults CHECK(grade > 0 AND grade <= 12)
+
 );
