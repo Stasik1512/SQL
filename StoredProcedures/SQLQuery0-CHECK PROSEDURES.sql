ae09d4c3bc599cff3ed08a3bf0216e1735d6a667
Nekit K. <14nikita@list.ru>
2026-04-21 19:03:31 +0500

    Stored Procedures

---
+++ StoredProcedures/SQLQuery0-CHECK PROSEDURES.sql
@@ -0,0 +1,9 @@
+--SQLQuery0-CHECK PROSEDURES
+
+USE P_421_Import;
+
+--DELETE FROM Schedule;
+
+EXECUTE sp_InsertSchedule N'P_421', N'%MS SQL Server',N'Юыху', N'2026-03-26',N'14:00'
+
+EXECUTE sp_SelectSchedule;
---
+++ StoredProcedures/SQLQuery2 INSER lesson.sql
@@ -0,0 +1,25 @@
+ --SQLQuery2 INSER lesson
+ -- проверки на возможность занятности группы или препода
+ USE P_421_Import
+ GO
+ ALTER PROCEDURE sp_InsertLesson
+	@group AS INT,
+	@discipline AS SMALLINT,
+	@teacher AS SMALLINT,
+	@date AS DATE,
+	@time AS TIME(0) OUTPUT,
+	@lesson_number AS TINYINT OUTPUT
+
+AS
+BEGIN
+
+	IF NOT EXISTS (SELECT lesson_id FROM Schedule WHERE [group]=@group AND [date] = @date AND [time]=@time)
+	BEGIN
+		INSERT Schedule([group], discipline, teacher,[date], [time], [spent])
+
+		VALUES (@group, @discipline,@teacher, @date, @time, IIF(@date <GETDATE(),1,0))
+
+		SET @time = DATEADD(MINUTE, 95, @time); 
+		SET @lesson_number += 1;
+	END
+END
